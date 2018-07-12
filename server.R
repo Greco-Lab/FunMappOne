a1d1a07d1b8d31a1a5db2f7b40a0c919c32426a8
grecolab <grecolab.fi@gmail.com>
2018-07-12 17:18:09 +0300

    Update server.R

--- server.R
+++ server.R
@@ -440,8 +440,7 @@ shinyServer(function(input, output,session) {
     
     #Test plot
     #gVars$toPlot = as.grob(ggplot(mtcars, aes(x = carb)) + geom_bar())
-    gVars$toPlot = "value"
-    return()
+    #return()
     
     l1 = input$lev1
     l2 = input$lev2
@@ -515,6 +514,8 @@ shinyServer(function(input, output,session) {
     ########################################################
     #gVars$toPlot = plot_grid(path_mat = mat_to_Plot,path_hier = hier,experiment_ann =  gVars$exp_ann,discrete =  isDiscrete,level_col = as.numeric(input$level),square_colors=c(),color_leg=c(),path_text_size = 12,treat_text_size = 12)
 
+    gVars$toPlot = "value"
+    return()
     gVars$toPlot = plot_grid(path_mat = mat_to_Plot,path_hier = hier,experiment_ann =  gVars$exp_ann,discrete =  isDiscrete,level_col = max(1,as.numeric(input$level)-1),square_colors=c(),color_leg=c(),path_text_size = 12,treat_text_size = 12)
     
     
