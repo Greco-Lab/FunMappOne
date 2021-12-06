18d99869ab91199b4d89257425548c0e45e98815
angy89 <meanser@wks-96131-mac.ad.tuni.fi>
2021-12-06 16:08:06 +0100

    updated to gprofiler2

--- enrich_functions.R
+++ enrich_functions.R
@@ -185,14 +185,25 @@ enrich = function(x, type, org, pval, adjust_method,sig = TRUE, mis = 0, only_an
   }
   
   print("before gprofiler")
-  out = gProfileR::gprofiler(query = as.character(x[,1]),src_filter=type,organism=org,domain_size = domain_size,
-                             max_p_value = pval, correction_method = adjust_method,significant = sig,min_isect_size = mis)
   
-  #out = gProfileR::gprofiler(query = x[,1],src_filter=type,organism=org,max_p_value = pval,domain_size = "known")
+  out = gprofiler2::gost(query = as.character(x[,1]),sources = type,organism = org,domain_scope = domain_size,
+                          user_threshold = pval,correction_method = adjust_method,significant = sig, evcodes = TRUE)
+  out = out$result
+  
+  if(is.null(out)) out = data.frame(query= character(), significant= integer(), p_value= integer(), term_size = integer(),            
+                                    query_size =  integer(), intersection_size =  integer(),precision =  integer(),recall =  integer(),               
+                                    term_id = character(),source = character(),term_name = character(),effective_domain_size = integer(),
+                                    source_order = integer(),parents = character(),evidence_codes = character(),intersection = character())
+  
   print(out)
   
-  ##print("after gprofiler")
-  out=out[,c("term.id","intersection","p.value","p.value","term.name")]
+  ### UPDATE gprofiler version
+  # out = gProfileR::gprofiler(query = as.character(x[,1]),src_filter=type,organism=org,domain_size = domain_size,
+  #                            max_p_value = pval, correction_method = adjust_method,significant = sig,min_isect_size = mis)
+
+  # out=out[,c("term.id","intersection","p.value","p.value","term.name")]
+  out=out[,c("term_id","intersection","p_value","p_value","term_name")]
+  
   colnames(out) = c( "annID","gID","pValue","pValueAdj","Description")
   
   print(out)
