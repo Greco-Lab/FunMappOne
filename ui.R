76e04b87bb13cd87bc60d5dc52bace4a34f3bf68
angy89 <meanser@wks-96131-mac.ad.tuni.fi>
2021-12-06 12:59:48 +0100

    add functions into extendShinyjs

--- ui.R
+++ ui.R
@@ -2,6 +2,7 @@ library(shiny)
 library(shinyjs)
 library(shinyBS)
 
+
 jsCode <- "
 shinyjs.disableTab = function(name) {
   var tab = $('.nav li a[data-value=' + name + ']');
@@ -63,7 +64,7 @@ z-index: 1000000;
 }"
 fluidPage(
   useShinyjs(),
-  extendShinyjs(text=jsCode),
+  extendShinyjs(text=jsCode,functions = c("disableTab","enableTab","addCustomTooltip","removeCustomTooltip")),
   #tags$head(tags$script(src="resizing.js")),
   inlineCSS(appCSS),
   hidden(div(id="loading-content",
